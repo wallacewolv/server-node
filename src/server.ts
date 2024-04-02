2a822dbe9488238cbeeee7f9edad489ca1b76223
wallace <wallace_wolv@hotmail.com>
2024-04-01 22:56:45 -0300

    feat: Adicionando fastify

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,11 @@
-console.log('Hello NLW');
+import fastify from "fastify";
 
+const app = fastify();
+
+app.get('/', () => {
+  return 'Hello NLW Unite';
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTP Server runnng!');
+});
