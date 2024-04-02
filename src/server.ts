e9e52c8309f5befee024288a0ec72b74c16518f8
wallace <wallace_wolv@hotmail.com>
2024-04-02 00:12:59 -0300

    feat: Testando http Client

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import fastify from "fastify";
 
 const app = fastify();
 
-app.get('/', () => {
-  return 'Hello NLW Unite';
+app.post('/events', (request, reply) => {
+  console.log(request.body);;
 });
 
 app.listen({ port: 3333 }).then(() => {
