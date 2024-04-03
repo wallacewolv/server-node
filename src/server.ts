5f50cffed5958cb3101d0bca471e1c853adb877a
wallace <wallace_wolv@hotmail.com>
2024-04-02 23:10:34 -0300

    feat: Inserindo slug e retornando erro caso o mesmo ja exista

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import { generateSlug } from "./utils/generate-slug";
 
 const app = fastify();
 
@@ -15,14 +16,30 @@ app.post('/events', async (request, reply) => {
     maximumAttendees: z.number().int().positive().nullable(),
   });
 
-  const data = createEventSchema.parse(request.body);
+  const {
+    title,
+    details,
+    maximumAttendees,
+  } = createEventSchema.parse(request.body);
+
+  const slug = generateSlug(title);
+
+  const eventWithSameSlug = await prisma.event.findUnique({
+    where: {
+      slug,
+    }
+  });
+
+  if (eventWithSameSlug !== null) {
+    throw new Error('Another event with same title already exists.')
+  }
 
   const event = await prisma.event.create({
     data: {
-      title: data.title,
-      details: data.details,
-      maximumAttendees: data.maximumAttendees,
-      slug: new Date().toISOString(),
+      title,
+      details,
+      maximumAttendees,
+      slug,
     }
   });
 
