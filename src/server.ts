e22b91b62e072ce2f9aff9493d9e3566c26071e0
wallace <wallace_wolv@hotmail.com>
2024-04-02 23:25:11 -0300

    feat: Adicionando schema para request e response

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,59 @@
 import fastify from "fastify";
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
 import { generateSlug } from "./utils/generate-slug";
 
 const app = fastify();
 
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
 const prisma = new PrismaClient({
   log: ['query'],
 });
 
-app.post('/events', async (request, reply) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  });
+app
+  .withTypeProvider<ZodTypeProvider>()
+  .post('/events', {
+    schema: {
+      body: z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+      }),
+      response: {
+        201: z.object({
+          eventId: z.string().uuid(),
+        }),
+      },
+    },
+  }, async (request, reply) => {
+    const { title, details, maximumAttendees } = request.body;
 
-  const {
-    title,
-    details,
-    maximumAttendees,
-  } = createEventSchema.parse(request.body);
+    const slug = generateSlug(title);
 
-  const slug = generateSlug(title);
+    const eventWithSameSlug = await prisma.event.findUnique({
+      where: {
+        slug,
+      }
+    });
 
-  const eventWithSameSlug = await prisma.event.findUnique({
-    where: {
-      slug,
+    if (eventWithSameSlug !== null) {
+      throw new Error('Another event with same title already exists.')
     }
+
+    const event = await prisma.event.create({
+      data: {
+        title,
+        details,
+        maximumAttendees,
+        slug,
+      }
+    });
+
+    return reply.status(201).send({ eventId: event.id });
   });
-
-  if (eventWithSameSlug !== null) {
-    throw new Error('Another event with same title already exists.')
-  }
-
-  const event = await prisma.event.create({
-    data: {
-      title,
-      details,
-      maximumAttendees,
-      slug,
-    }
-  });
-
-  return reply.status(201).send({ eventId: event.id });
-});
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTP Server runnng!');
