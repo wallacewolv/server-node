21cd5c1cdf4675486e32e2a46a7ee105f835bd03
wallace <wallace_wolv@hotmail.com>
2024-04-03 00:17:34 -0300

    feat: Tratamento de erro para quando o email ja etiver cadastrado no evento

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -25,6 +25,19 @@ export async function registerForEvent(app: FastifyInstance) {
       const { eventId } = request.params;
       const { name, email } = request.body;
 
+      const attendeeFromEmail = await prisma.attendee.findUnique({
+        where: {
+          eventId_email: {
+            email,
+            eventId,
+          },
+        },
+      });
+
+      if (attendeeFromEmail !== null) {
+        throw new Error('This e-mail is already registerd for this event.');
+      }
+
       const attendee = await prisma.attendee.create({
         data: {
           name,
